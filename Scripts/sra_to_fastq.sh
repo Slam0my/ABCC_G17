c8afdc67e446866a6e54d91751450ebbe41ba990
Sarah Lam <148975195+Slam0my@users.noreply.github.com>
2025-12-03 19:37:12 +0000

    Create sra_to_fastq.sh

---
+++ Scripts/sra_to_fastq.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --output=/scratch/grp/msc_appbio/Group17_ABCC/sra_job%j.out
+#SBATCH --job-name=sra_to_fastq
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --time=01:00:00
+#SBATCH --partition=msc_appbio
+
+module load sra-tools/3.0.3-gcc-13.2.0
+
+echo "Start of SRA conversion to fastq"
+
+cd /scratch/grp/msc_appbio/Group17_ABCC/SRA_raw_data
+
+fasterq-dump --split-files -O /scratch/grp/msc_appbio/Group17_ABCC/fastq_files *.sra
+
+echo "End of fastq conversion"
